f7a3c57e2402f6edd384cef0d6b022fbf83013c4
racoobina <54292141+racoobina@users.noreply.github.com>
2020-11-30 18:33:12 +0300

    Add files via upload
    тут еще __init__ не подгрузился

---
+++ svggnet.py
@@ -0,0 +1,58 @@
+# Импорт модулей Керас
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+
+class sVGGNet:
+	@staticmethod
+	def build(width, height, depth, classes):
+		# Инициализируем последовательную модель, размер входного изображения и размер канала
+		model = Sequential()
+		inputShape = (height, width, depth)
+		chanDim = -1
+
+		# слои CONV => RELU => POOL
+		model.add(Conv2D(32, (3, 3), padding="same",
+			input_shape=inputShape))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(3, 3)))
+		model.add(Dropout(0.25))
+
+		# (CONV => RELU) * 2 => POOL
+		model.add(Conv2D(64, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(64, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# (CONV => RELU) * 2 => POOL
+		model.add(Conv2D(128, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(128, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# FC => RELU
+		model.add(Flatten())
+		model.add(Dense(1024))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization())
+		model.add(Dropout(0.5))
+
+		# softmax
+		model.add(Dense(classes))
+		model.add(Activation("softmax"))
+
+		return model
